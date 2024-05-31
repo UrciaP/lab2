59f6ebf455743cb96f9c0f7b3ce7f42c37658cf9
Urcia-Tw <urciapelaezluisalex@gmail.com>
2024-05-30 23:38:12 -0500

    tambo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,10 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('titulo')</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
         <!-- <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet"> -->
         <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
         <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <style>
             /* Asegúrate de que solo los enlaces dentro de elementos con la clase 'activo' tengan los estilos aplicados */
             .activo > a {
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 
-$servicios=[
-    ['titulo'=>'Servicio 1'],
-    ['titulo'=>'Servicio 2'],
-    ['titulo'=>'Servicio 3'],
-    ['titulo'=>'Servicio 4'],
-    ['titulo'=>'Servicio 5'],
-];
+// $servicios=[
+//     ['titulo'=>'Servicio 1'],
+//     ['titulo'=>'Servicio 2'],
+//     ['titulo'=>'Servicio 3'],
+//     ['titulo'=>'Servicio 4'],
+//     ['titulo'=>'Servicio 5'],
+// ];
 
 
 Route::view('/','home')->name('home');
 Route::view('nosotros','nosotros')->name('nosotros');
+
 // controlador 1
-// Route::get('servicios','App\Http\Controllers\ServiciosController@servicios' )->name('servicios');
+Route::get('servicios','App\Http\Controllers\ServiciosController@servicios' )->name('servicios');
 
 // controlador 2
 // Route::get('servicios','App\Http\Controllers\Servicios2Controller@index' )->name('servicios');
@@ -29,28 +30,5 @@ Route::view('contacto','contacto')->name('contacto');
 
 
 
-
-Route::view('servicios','servicios', compact('servicios'))->name('servicios');
-
-
-
-
-// Route::get('servicios/{opcional?}', function ($opcional = null) {
-//     return view('servicios', ['opcional' => $opcional]);
-// })->where('opcional', '[A-Za-z]+');
-
-// Route::get('proyectos/{opcional?}', function ($opcional = null) {
-//     return view('proyectos', ['opcional' => $opcional]);
-// })->where('opcional', '[A-Za-z]+');
-
-// Route::get('clientes/{opcional?}', function ($opcional = null) {
-//     return view('clientes', ['opcional' => $opcional]);
-// })->where('opcional', '[A-Za-z]+');
-
-// Route::get('blog/{opcional?}', function ($opcional = null) {
-//     return view('blog', ['opcional' => $opcional]);
-// })->where('opcional', '[0-9]+');
-
-// Route::get('contacto', function () {
-//     return view('contacto');
-// });
+//el q use
+// Route::view('servicios','servicios', compact('servicios'))->name('servicios');
